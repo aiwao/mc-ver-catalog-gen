a44a496f3d5b4162f005f035b70dbbc9b7b98d6f
aiwao <233898056+aiwao@users.noreply.github.com>
2026-01-02 11:38:12 +0900

    ver catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,123 +1,149 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState} from "react"
 import './App.css'
-import {Box, Button, FormControlLabel, Slider, Switch} from "@mui/material"
+import {Box, Button, FormControlLabel, IconButton, Slider, Switch, Tooltip} from "@mui/material"
 import {Controller, type SubmitHandler, useForm} from "react-hook-form"
-import {getApiVersions, getGameVersions, getYarnVersions} from "./fabric/api.ts"
+import {
+  type GameVersion,
+  getApiVersionForMinecraft,
+  getGameVersions,
+  getMinecraftYarnVersions, type YarnVersion
+} from "./fabric/api.ts"
+import {ArrowForward, ContentCopy} from "@mui/icons-material"
 
 type GenFormData = {
   versionRange: number[],
   yarn: boolean,
   minecraft: boolean,
-  fabric: boolean,
+  api: boolean,
 }
 
 function App() {
   const [verMap, setVerMap] = useState<Map<number, string>>(new Map())
-  const [yarnVerMap, setYarnVerMap] = useState<Map<string, string>>
-  const [fabricVerMap, setFabricVerMap] = useState<Map<string, string>>
+  const [yarnVerMap, setYarnVerMap] = useState<Map<number, string>>(new Map())
+  const [apiVerMap, setApiVerMap] = useState<Map<number, string>>(new Map())
   const [error, setError] = useState<string | null>(null)
   const [loading, setLoading] = useState(true)
+  const [rangeText, setRangeText] = useState("")
   const [maxValue, setMaxValue] = useState(0)
-  const [catalog, setCatalog] = useState<Catalog | null>(null)
-
-  const { control, handleSubmit } = useForm<GenFormData>({
+  const { control, handleSubmit, setValue } = useForm<GenFormData>({
     defaultValues: {
       versionRange: [0, 0],
       yarn: true,
       minecraft: true,
-      fabric: true
+      api: true
     }
   })
+  const [catalog, setCatalog] = useState<string | null>(null)
 
-  const catalogToText = (value: Catalog): string => {
-    const versions = value.versions
-    const versionSection = "[versions]\n"
-      +versions
-        .map(v => v.version.map(v2 => v.prefix+v2.replaceAll(".", "")))
-        .join("\n")
-
-    const libraries = value.libraries
-    const librarySection = "[libraries]\n"
-      +libraries
-        .map(v => v.versions.version.map(v2 => `{ module = ${v.module}, version.ref = "${v.versions.version+v2}" }`))
-        .join("\n")
-
-    return versionSection+"\n\n"+librarySection
-  }
-
-  const getVerText = (value: number): string => {
-    const ver = verMap.get(value)
+  const getVerText = (map: Map<number, string>, value: number): string => {
+    const ver = map.get(value)
     if (ver) return ver
     return "undefined"
   }
 
+  const handleCopy = () => {
+    if (catalog)
+      navigator.clipboard.writeText(catalog)
+  }
+
   const onGenSubmit: SubmitHandler<GenFormData> = (data) => {
-    const yarnVersions: string[] = []
-    const mcVersions: string[] = []
-    const fabricVersions: string[] = []
+    const verSectionMap: Map<string, string[]> = new Map()
+    const libSectionMap: Map<string, string[]> = new Map()
+    const addCatalog = (map: Map<number, string>, index: number, prefix: string, maven: string, mcVer: string) => {
+      const ver = map.get(index)
+      if (ver) {
+        const verSection = verSectionMap.get(prefix) ?? []
+        const name = `${prefix}-mc${mcVer.replace(/\D/g, "")}`
+        verSection.push(`${name} = "${ver}"`)
+        verSectionMap.set(prefix, verSection)
+        const libSection = libSectionMap.get(prefix) ?? []
+        libSection.push(`${name} { module = "${maven}", version.ref = "${name}" }`)
+        libSectionMap.set(prefix, libSection)
+      }
+    }
     for (let i = data.versionRange[0]; i <= data.versionRange[1]; i++) {
       const mcVer = verMap.get(i)
-      if (!mcVer) continue
-
-      if (data.yarn) {
-        const ver = yarnVerMap.get(mcVer)
-        yarnVersions.push(ver)
-      }
-      if (data.minecraft) {
-        mcVersions.push(mcVer)
-      }
-      if (data.fabric) {
-        const ver = fabricVerMap.get(mcVer)
-        fabricVersions.push(ver)
+      if (mcVer) {
+        if (data.minecraft) addCatalog(verMap, i, "minecraft", "com.mojang:minecraft", mcVer)
+        if (data.yarn) addCatalog(yarnVerMap, i, "yarn", "net.fabricmc:yarn", mcVer)
+        if (data.api) addCatalog(apiVerMap, i, "fabric", "net.fabricmc.fabric-api:fabric-api", mcVer)
       }
     }
-    const yarnVer = {
-      prefix: "yarn",
-      version: yarnVersions
+    if (verSectionMap.size != 0 && libSectionMap.size != 0) {
+      const verSection: string[] = []
+      const libSection: string[] = []
+      for (const value of [...verSectionMap.values()]) {
+        verSection.push(...value)
+      }
+      for (const value of [...libSectionMap.values()]) {
+        libSection.push(...value)
+      }
+      setCatalog(`[versions]\n${verSection.join("\n")}\n\n[libraries]\n${libSection.join("\n")}`)
     }
-    const mcVer = {
-      prefix: "mc",
-      version: mcVersions
-    }
-    const fabricVer = {
-      prefix: "fabric",
-      version: fabricVersions
-    }
-    setCatalog({
-      versions: [
-        yarnVer,
-        mcVer,
-        fabricVer,
-      ],
-      libraries: [
-        {
-          module: "net.fabricmc:yarn",
-          versions: yarnVer
-        },
-        {
-          module: "com.mojang:minecraft",
-          versions: mcVer
-        },
-        {
-          module: "net.fabricmc.fabric-api:fabric-api",
-          versions: fabricVer
-        }
-      ]
-    })
   }
 
   useEffect(() => {
     const fetchVersions = async () => {
-      const yarnVersions = await getYarnVersions()
-      const mcVersions = await getGameVersions()
+      let mcVersions: GameVersion[]
+      try {
+        mcVersions = await getGameVersions()
+      } catch (e) {
+        console.error(e)
+        throw new Error("failed to fetch minecraft versions")
+      }
+      const stableVersions = mcVersions
+        .filter(v => v.stable)
+        .reverse()
+      const tempVerMap = new Map<number, string>()
+      const tempYarnVerMap = new Map<number, string>()
+      const tempApiVerMap = new Map<number, string>()
+
+      for (let i = 0; i < stableVersions.length; i++) {
+        const gameVer = stableVersions[i]
+        const gameVerStr = gameVer.version
+        tempVerMap.set(i, gameVerStr)
+        let yarnVerForGameVer: YarnVersion[]
+        try {
+          yarnVerForGameVer = await getMinecraftYarnVersions(gameVerStr)
+        } catch (e) {
+          console.error(e)
+          throw new Error(`failed to fetch yarn version for ${gameVerStr}`)
+        }
+        if (yarnVerForGameVer.length != 0)
+          tempYarnVerMap.set(i, yarnVerForGameVer.shift()!.version)
+        let apiVerForGameVer: string
+        try {
+          apiVerForGameVer = await getApiVersionForMinecraft(gameVerStr)
+        } catch (e) {
+          console.error(e)
+          throw new Error(`failed to fetch fabric-api version for ${gameVerStr}`)
+        }
+        tempApiVerMap.set(i, apiVerForGameVer)
+      }
+
+      const versionSize = tempVerMap.size-1
+      setMaxValue(versionSize)
+      setValue("versionRange", [0, versionSize])
+      setVerMap(tempVerMap)
+      setYarnVerMap(tempYarnVerMap)
+      setApiVerMap(tempApiVerMap)
+      setRangeText(`${getVerText(tempVerMap, 0)} - ${getVerText(tempVerMap, versionSize)}`)
     }
     fetchVersions()
+      .catch((e) => setError(e.message))
+      .finally(() => setLoading(false))
   }, [])
 
   return (
     <>
       {loading &&
         <p>Loading</p>
+      }
+      {error &&
+        <>
+          <p>{error}</p>
+          <Button onClick={() => window.location.reload()}>Retry</Button>
+        </>
       }
       {!loading && !error &&
         <Box
@@ -126,7 +152,7 @@ function App() {
           onSubmit={handleSubmit(onGenSubmit)}
         >
           <Box>
-            <p>Versions</p>
+            <p>{rangeText}</p>
             <Controller name="versionRange" control={control} render={({ field }) => (
               <Slider
                 {...field}
@@ -134,8 +160,11 @@ function App() {
                 min={0}
                 max={maxValue}
                 value={field.value}
-                onChange={field.onChange}
-                valueLabelFormat={(v) => getVerText(v)}
+                onChange={(e, v) => {
+                  setRangeText(`${getVerText(verMap, v[0])} - ${getVerText(verMap, v[1])}`)
+                  field.onChange(e)
+                }}
+                valueLabelFormat={(v) => getVerText(verMap, v)}
               />
             )}>
             </Controller>
@@ -157,26 +186,48 @@ function App() {
               />
             )}>
             </Controller>
-            <Controller name="fabric" control={control} render={({ field }) => (
+            <Controller name="api" control={control} render={({ field }) => (
               <FormControlLabel
                 {...field}
                 control={<Switch checked={field.value} defaultChecked />}
-                label="Fabric"
+                label="Fabric API"
               />
             )}>
             </Controller>
           </Box>
-          <Button type="submit">Generate</Button>
+          <Button variant="contained" type="submit" endIcon={<ArrowForward/>}>Generate</Button>
+          {catalog &&
+            <Box
+              sx={{
+                position: "relative",
+                backgroundColor: "#1e1e1e",
+                color: "#fff",
+                p: 2,
+                borderRadius: 1,
+                fontFamily: "monospace",
+                whiteSpace: "pre",
+              }}
+            >
+              <Tooltip title="Copy">
+                <IconButton
+                  onClick={handleCopy}
+                  sx={{
+                    position: "absolute",
+                    top: 8,
+                    right: 8,
+                    color: "#fff",
+                  }}
+                  size="small"
+                >
+                  <ContentCopy fontSize="inherit" />
+                </IconButton>
+              </Tooltip>
+              <pre style={{ margin: 0, textAlign: "start", padding: 2, overflowX: "scroll" }}>
+                <code>{catalog}</code>
+              </pre>
+            </Box>
+          }
         </Box>
-      }
-      {error &&
-        <>
-          <p>{error}</p>
-          <Button onClick={() => window.location.reload()}>Retry</Button>
-        </>
-      }
-      {catalog &&
-        catalogToText(catalog)
       }
     </>
   )
